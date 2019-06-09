6f077f30b04902df999afd895947c04f166b2d4c
Cloudmersive <35204726+Cloudmersive@users.noreply.github.com>
2019-06-09 15:26:07 -0700

    Added

--- CloudmersiveReceiptOCR/MainWindow.xaml.cs
+++ CloudmersiveReceiptOCR/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace CloudmersiveReceiptOCR
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
